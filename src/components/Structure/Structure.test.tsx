711f7dea7bc95c1f7f44e0c8cd5375745a4f569e
lfilella97 <lfilella97@gmail.com>
2023-03-14 00:26:01 +0100

    Test Structure component

---
+++ src/components/Structure/Structure.test.tsx
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../testUtil/renderWithProviders";
+import Structure from "./Structure";
+
+describe("Given the component Structure", () => {
+  describe("When it is rendered with the `Aljub del mas de Roer`", () => {
+    test("Then it should show a Structure with image", () => {
+      const structure = {
+        elevation: "455",
+        name: "Bassa del mas de Roer",
+        type: "Water",
+        location: "La granadella",
+        image:
+          "https://sfxfnjejlztsnoxyochi.supabase.co/storage/v1/object/public/structures/Aljub%20del%20mas%20de%20Roer.jpg",
+      };
+
+      renderWithProviders(<Structure structure={structure} />);
+
+      const image = screen.getByRole("img");
+
+      expect(image).toBeInTheDocument();
+    });
+  });
+});
