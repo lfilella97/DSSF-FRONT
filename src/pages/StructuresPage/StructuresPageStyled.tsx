d2b07ed7525530e18b96a0482acd84e487830dfc
lfilella97 <lfilella97@gmail.com>
2023-03-14 11:13:07 +0100

    Refactor Structure & Structures to add grid and make it responsive

--- src/pages/StructuresPage/StructuresPageStyled.tsx
+++ src/pages/StructuresPage/StructuresPageStyled.tsx
@@ -3,11 +3,17 @@ import styled from "styled-components";
 const StructuresStyled = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+  ul {
+    display: grid;
+    grid-auto-rows: auto;
+    grid-template-columns: repeat(auto-fill, minmax(310px, 1fr));
+    gap: 10px;
+  }
 
   .structures > li {
+    display: flex;
+    justify-content: center;
     padding: 0 0 20px;
   }
 `;
